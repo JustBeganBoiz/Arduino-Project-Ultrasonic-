15c45e730dd66149c8a100bbc0ef9610a8e0cf57
OpenProgrammer <62532588+JustBeganBoiz@users.noreply.github.com>
2026-06-17 20:28:03 +0530

    Adding the Arduino File

---
+++ first.ino
@@ -0,0 +1,166 @@
+#include <Servo.h>
+
+const int LED = 3;
+int value = 0;
+int fadeAmount = 5;  
+
+Servo myservo;
+int pos = 0;
+
+const int button = 2;
+
+const int Buzz = 6;
+
+const int Echo = 10;
+const int Trig = 9;
+
+long duration;
+int distance;
+
+bool Objectinside = false;
+unsigned long timeinside;
+bool done = false;
+
+bool syson = true;
+bool lastbuttonstate = HIGH;
+
+bool Ledstate = false;
+long int previousblink;
+const int blinkinterval = 300;
+
+void setup() {
+
+  myservo.attach(11);
+
+  pinMode(button, INPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(Echo, INPUT);
+  pinMode(Trig, OUTPUT);
+  pinMode(Buzz, OUTPUT);
+   
+  digitalWrite(LED, LOW);
+  delay(100);
+
+  digitalWrite(LED, HIGH);
+  delay(500);
+
+  digitalWrite(LED, LOW);
+  delay(500);
+
+  Serial.begin(115200);
+}
+
+void loop() {
+
+  if(syson){
+
+    digitalWrite(Trig, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(Trig, HIGH);
+    delayMicroseconds(10);
+
+    digitalWrite(Trig, LOW);
+
+    duration = pulseIn(Echo, HIGH, 30000);   
+
+    distance = (duration * 0.0343) / 2;
+
+    Serial.print("Distance: ");
+    Serial.println(distance);                
+
+    analogWrite(LED, value);                 
+
+    value = value + fadeAmount; 
+
+    if (value >= 255 || value <= 0) {
+      fadeAmount = -fadeAmount;              
+    }
+
+    if (distance <= 150 && !Objectinside) {
+        Objectinside = true;
+        done = false;
+        timeinside = millis();
+    }
+
+    if (Objectinside && !done) {
+
+        if (millis() - timeinside < 10000) {
+
+            if(millis() - previousblink >= blinkinterval){
+              previousblink = millis();
+
+              Ledstate = !Ledstate;
+
+              if(Ledstate){
+                analogWrite(LED, 255);
+              }
+              else{
+                analogWrite(LED, 0);
+              }
+            }
+
+            int freq = map(distance, 0, 150, 2500, 1000);
+            freq = constrain(freq, 1000, 2500);
+            tone(Buzz, freq);
+        }
+        else {
+            done = true;
+            analogWrite(LED, 0);
+            delay(100);
+
+            analogWrite(LED, 255);
+            delay(200);
+
+            analogWrite(LED, 0);
+            delay(200);
+            
+            analogWrite(LED, 250);
+            delay(200);
+
+            analogWrite(LED, 0);
+            delay(200);
+
+            noTone(Buzz);
+        }
+    }
+
+      if (distance > 150 && Objectinside) {
+          Objectinside = false;
+          done = false;
+
+          analogWrite(LED, 0);
+          delay(100);
+
+          analogWrite(LED, 255);
+          delay(200);
+
+          analogWrite(LED, 0);
+          delay(200);
+            
+          analogWrite(LED, 250);
+          delay(200);
+
+          analogWrite(LED, 0);
+          delay(200);
+
+          noTone(Buzz);
+      }
+
+    delay(100);
+  }
+  bool currentstate = digitalRead(button);
+
+  if (currentstate == LOW && lastbuttonstate == HIGH) {
+      syson = !syson;
+      delay(50);    
+  }
+
+  lastbuttonstate = currentstate;
+
+  if (!syson) {
+    noTone(Buzz);
+    analogWrite(LED, 0);
+    return;
+  }
+}
